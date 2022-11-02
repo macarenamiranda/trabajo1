0e195588914af6924e79f274a885f967f5e338df
macarenamiranda <106710692+macarenamiranda@users.noreply.github.com>
2022-11-01 23:33:25 -0300

    Add files via upload

---
+++ trabajo 1/js/index.js
@@ -0,0 +1,15 @@
+// Ciclo de instrucciones con for
+
+const num1 = 10
+
+for (let i = 0;  i    num1; i++ ){
+
+    //condicional
+
+    if( i == num){  
+alert(`llegamos al número diez`)
+ }else{
+alert(`Iteramos hasta llegar al 10 estamos en el número ${i}`)
+}
+
+ }  
